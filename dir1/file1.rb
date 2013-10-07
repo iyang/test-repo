3107fb67d35425c9b606abaf3030e50960b760aa
Ryan Liang <zhicheng.liang@oracle.com>
2013-10-07 10:55:33 -0400

    added code for debugging test

--- dir1/file1.rb
+++ dir1/file1.rb
@@ -1,3 +1,8 @@
+=begin
+  
+
+  
+
 ======== This is file 1 =========
 this is line 1
 this is line 2 
@@ -8,3 +13,24 @@ this is line 5.5
 this is line 6
 
 
+=end
+
+def main
+  puts "welcome! This is for debugging test"
+  foo("Joe Doe")
+  puts bar(10, 2)
+end
+
+def foo(name)
+  puts "from foo " + name
+end
+
+def bar(num1, num2)
+  minus = num1 - num2
+  plus  = num1 + num2
+  mult  = num1 * num2
+  div   = num1 / num2 if num2 != 0
+  result = minus + plus + mult + div
+end
+
+main
