3b1c0d9f93e752e8f02921b700c4a953e3d510a4
Ryan Liang <zhicheng.liang@oracle.com>
2013-10-07 11:03:40 -0400

    removed unused comments

--- dir2/file2.rb
+++ dir2/file2.rb
@@ -1,10 +1,3 @@
-# ======== This is file 2 =========
-# this is line 1
-# this is line 2 
-# this is line 3
-# this is line 4
-# this is line 5
-
 def test
   puts 'this is from file2 line 1'
   puts 'this is from file2 line 2'
